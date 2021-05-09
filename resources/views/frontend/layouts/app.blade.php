35afc9d1cf088a0ba9b00dba64611e3e652f18ff
Sanilblank <sanilmanandhar@gmail.com>
2021-05-09 13:59:59 +0545

    messenger bot

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Ogani Template">
-    <meta name="keywords" content="Ogani, unica, creative, html">
+    <meta name="description" content="Tajaa Mandi:Powered by Revotech Nepal, Taja Mandi is an internet shopping initiation. A marketplace for eateries; vendors & consumers. We provide our customers with the best deals from the market. Connect with us for the best deals from around the country. Compare, click and buy.">
+    <meta name="keywords" content="Taja Mandi | Groceries E-store| E-commerce">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TajaMandi | All your Home Groceries.</title>
@@ -31,19 +31,47 @@
     <style>
         .hero__search__form{
             position: absolute;
+            max-width: 92%;
         }
+        .single-hero-items.set-bg{
+            height:500px;
+        }
+        /*@media only screen and (max-width: 767px)*/
+        /*    .hero__search__form {*/
+        /*        width: 92%;*/
+        /*}*/
         /* @media (max-width: 1200px) {
             .hero__search__form{
             position: absolute;
         }
          } */
     </style>
+
+        <!-- Facebook Pixel Code -->
+    <script type="text/javascript">
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2888318151426386');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=2888318151426386&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Facebook Pixel Code -->
 </head>
 
 <body>
     @include('frontend.layouts.includes.header')
 
     @yield('content')
+
+    <div id="fb-root"></div>
 
     @include('frontend.layouts.includes.footer')
     @include('frontend.layouts.includes.modal')
@@ -63,7 +91,8 @@
     <!-- Include AlgoliaSearch JS Client and autocomplete.js library -->
   <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
   <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
-  <script src="{{ asset('frontend/js/algolia.js') }}"></script>
+   <script src="{{ asset('frontend/js/algolia.js') }}"></script>
+
     <script>
         function showPasswordForm(){
         $('.loginBox').fadeOut('fast',function(){
@@ -98,7 +127,6 @@
         });
         $('.error').removeClass('alert alert-danger').html('');
     }
-
 
     function openLoginModal(){
         showLoginForm();
@@ -140,6 +168,31 @@
         $('.alert').fadeOut('slow');
     }, 5000); // 5 secs
 </script>
+
+<!-- Messenger Chat Plugin Code -->
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      </script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution="page_inbox"
+        page_id="110315501202576">
+      </div>
+
 @stack('scripts')
 </body>
 
