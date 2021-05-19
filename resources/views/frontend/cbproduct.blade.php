e5a3874daaeab576acfe04886aa7f1fc67598f19
nischalshk <nischal.shk@gmail.com>
2021-05-19 17:52:42 +0545

    cb product responsive

--- resources/views/frontend/cbproduct.blade.php
+++ resources/views/frontend/cbproduct.blade.php
@@ -4,15 +4,13 @@
 
 
     <!-- Breadcrumb Section Begin -->
-    <section class="breadcrumb-section set-bg" data-setbg="{{asset('frontend/img/breadcrumb.jpg')}}">
+    <section class="breadcrumb-section set-bg mt-3" style="background-repeat: no-repeat; background-size:cover;background-position:top center;" data-setbg="{{asset('frontend/img/breadcrumb.jpg')}}">
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
             <div class="breadcrumb__text">
               <h2>Vegetable’s Package</h2>
               <div class="breadcrumb__option">
-                <a href="./index.html">Home</a>
-                <a href="./index.html">Vegetables</a>
                 <span>Vegetable’s Package</span>
               </div>
             </div>
